468643ddb26223f58871c47f99cfafc59cfebcf2
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-06-13 10:02:32 +0800

    Create InventoryModel.cs

---
+++ Assets/InventoryModel.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InventoryModel : MonoBehaviour
+{
+
+    [SerializeField] InventorySlot slot;
+    [SerializeField] int maxInventorySize;
+
+    public List<InventorySlot> slotList = new List<InventorySlot>();
+    public int maxInventorySlots = 16;
+    public List<SlotData> playerInventory = new List<SlotData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < maxInventorySize; i++) 
+        {
+            InventorySlot slotToBeAdded = Instantiate(slot);
+
+           slotToBeAdded.transform.SetParent(this.transform,false);
+           slotList.Add(slotToBeAdded);
+        }
+
+        UpdateSlots(); 
+
+    }
+
+    public void UpdateSlots()
+    {
+        for (int i = 0; i < maxInventorySize; i++)
+        {
+            if (playerInventory[i] != null)
+            {
+                slotList[i].item = playerInventory[i].item;
+                slotList[i].amount = playerInventory[i].amount; // This should be adjusted as needed
+            }
+            else
+            {
+                slotList[i].item = null;
+            }
+
+        
+        }
+    }
+
+    public void InsertItem(InventoryItem item, int amount)
+    {
+        int overflow = amount;
+
+        foreach (var slot in slotList)
+        {
+            if (slot.item == item && slot.amount < item.maxStackSize)
+            {
+                int availableSpace = item.maxStackSize - slot.amount;
+                if (overflow <= availableSpace)
+                {
+                    slot.amount += overflow;
+                    overflow = 0;
+                    break;
+                }
+                else
+                {
+                    slot.amount += availableSpace;
+                    overflow -= availableSpace;
+                }
+
+                slot.UpdateSlot();
+            }
+        }
+
+        if (overflow > 0)
+        {
+            foreach (var slot in slotList)
+            {
+                if (slot.item == null)
+                {
+                    slot.item = item;
+                    if (overflow <= item.maxStackSize)
+                    {
+                        slot.amount = overflow;
+                        overflow = 0;
+                    }
+                    else
+                    {
+                        slot.amount = item.maxStackSize;
+                        overflow -= item.maxStackSize;
+                    }
+
+                    slot.UpdateSlot();
+                    break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("SLot data "+ playerInventory[0].item);
+    }
+}
