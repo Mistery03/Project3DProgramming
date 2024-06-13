2db342bf3a1558617feb1974bc86073fc108a6bd
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-06-13 11:18:01 +0800

    mouseEvent

--- Assets/InventoryController.cs
+++ Assets/InventoryController.cs
@@ -1,6 +1,8 @@
+using Inventory.Model;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class InventoryController : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class InventoryController : MonoBehaviour
     public List<SlotData> playerInventory = new List<SlotData>();
     public int maxInventorySlots = 16;
 
+    public ItemData testApple;
 
     // Update is called once per frame
     void Update()
@@ -26,5 +29,13 @@ public class InventoryController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.I))
             inventoryObject.SetActive(!inventoryObject.activeSelf);
+
+        if (Input.GetKeyDown(KeyCode.L))
+            inventoryModel.InsertItem(testApple, 1);
+    }
+
+    private void OnMouseEnter()
+    {
+        print("test");
     }
 }
