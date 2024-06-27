94dc33827f66cea5f3c3d0710f7da9b4ef0a9e0d
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-06-27 08:36:04 +0800

    DEfault chemical value

--- Assets/ChemicalList.cs
+++ Assets/ChemicalList.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ChemicalList : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public TextMeshProUGUI hydrogenAmountText;
+    public TextMeshProUGUI oxygenAmountText;
+    public TextMeshProUGUI carbonAmountText;
+    public TextMeshProUGUI mercuryAmountText;
+    public TextMeshProUGUI uraniumAmountText;
+
+    int defaultValue = 0;
     void Start()
     {
-        
+        hydrogenAmountText.text = defaultValue.ToString();
+        oxygenAmountText.text = defaultValue.ToString();
+        carbonAmountText.text = defaultValue.ToString();
+        mercuryAmountText.text = defaultValue.ToString();
+        uraniumAmountText.text = defaultValue.ToString();
+
     }
 
     // Update is called once per frame
