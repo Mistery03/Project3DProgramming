7a406da35d686be0666bb530a8b3cd12d725acf9
Elaine Sham <88269652+Mistery03@users.noreply.github.com>
2024-07-06 01:54:21 +0800

    Checkpoint 2

--- Assets/UI/Inventory/item.cs
+++ Assets/UI/Inventory/item.cs
@@ -5,17 +5,17 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
-public class item : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class Item : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     public Image image;
     Transform parentAfterDrag;
+    public Transform Canvas;
     public void OnBeginDrag(PointerEventData eventData)
     {
         image.raycastTarget = false;
         parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
+        transform.SetParent(Canvas);
         transform.SetAsLastSibling();
-        
         
     }
 
--- Assets/inventorySlot.cs
+++ Assets/inventorySlot.cs
@@ -7,92 +7,13 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
-public class InventorySlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class InventorySlot : MonoBehaviour, IDropHandler
 {
-    public ItemData item;
-    public int amount = 0;
-
-    [SerializeField] UnityEngine.UI.Image texture;
-    [SerializeField] TextMeshProUGUI amountText;
-    [SerializeField] GameObject itemTexture, itemAmount; //To have visible options
-
-    public InventoryModel inventoryModel;
-    public HotBarModel hotBarModel;
-   
-
-    void Start()
+    public void OnDrop(PointerEventData eventData)
     {
-    
-       // texture.GetComponent<UnityEngine.UI.Image>().sprite;
-    }
-    // Update is called once per frame
-    void Update()
-    {
-       
-        if(item != null) 
+        if(transform.childCount == 0)
         {
-            itemTexture.SetActive(true);
-            itemAmount.SetActive(true);
-            texture.sprite = item.ItemImage;
-            amountText.text = amount.ToString();
-        }else
-        {
-            itemTexture.SetActive(false);
-            itemAmount.SetActive(false);
-
+            
         }
-    }
-
-    public void UpdateSlot()
-    {
-        if (item != null)
-        {
-            itemTexture.SetActive(true);
-            itemAmount.SetActive(true);
-            texture.sprite = item.ItemImage;
-            amountText.text = amount.ToString();
-        }
-        else
-        {
-            itemTexture.SetActive(false);
-            itemAmount.SetActive(false);
-
-        }
-    }
-
-    public void ClearSlot()
-    {
-        item = null;
-        amount = 0;
-        UpdateSlot();
-    }
-
-  
-   
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-  
-        
-        Debug.Log(item.name);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if(eventData.button == PointerEventData.InputButton.Left)
-        {
-            if (hotBarModel.InsertItem(item, amount))
-                inventoryModel.Remove(item, amount,false);
-        }
-        else if (eventData.button == PointerEventData.InputButton.Right)
-        {
-            Debug.Log(item.name);
-            inventoryModel.Remove(item, 1);
-        }    
     }
 }
